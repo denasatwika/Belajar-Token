2f6f2fea2108b0abbe7256b12645dad32aa887e4
denasatwika <158028249+denasatwika@users.noreply.github.com>
2025-05-15 16:36:07 +0800

    First Commit

--- src/declarations/token/index.js
+++ src/declarations/token/index.js
@@ -1,7 +1,9 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
+
+// Imports and re-exports candid interface
 import { idlFactory } from './token.did.js';
 export { idlFactory } from './token.did.js';
-
+// CANISTER_ID is replaced by webpack based on node environment
 export const canisterId = process.env.TOKEN_CANISTER_ID;
 
 /**
@@ -13,6 +15,7 @@ export const canisterId = process.env.TOKEN_CANISTER_ID;
  export const createActor = (canisterId, options) => {
   const agent = new HttpAgent({ ...options?.agentOptions });
   
+  // Fetch root key for certificate validation during development
   if(process.env.NODE_ENV !== "production") {
     agent.fetchRootKey().catch(err=>{
       console.warn("Unable to fetch root key. Check to ensure that your local replica is running");
@@ -20,6 +23,7 @@ export const canisterId = process.env.TOKEN_CANISTER_ID;
     });
   }
 
+  // Creates an actor with using the candid interface and the HttpAgent
   return Actor.createActor(idlFactory, {
     agent,
     canisterId,
